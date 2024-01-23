ecb85d461e639d4997b25ada48bc350fe35eef18
KaustavRoy10 <kaustavroy49@gmail.com>
2024-01-23 18:39:19 +0530

    updated and added functions

--- data_processing.py
+++ data_processing.py
@@ -1,1 +1,12 @@
 print("Data processing for Fraud detection will happen here")
+
+a= 5
+b = 10
+def sum(a,b):
+    c=a+b
+    return c
+    print("Funtion summation")
+def mul(a,b):
+    d=a*b
+    print("Function mult")
+    return d
